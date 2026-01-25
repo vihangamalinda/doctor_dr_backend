3a28d3405dde6a0bf641bddb0c6ee0db1317d946
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-26 03:02:23 +0530

    feat: Disease stage Controller.

---
+++ dr/src/main/java/com/doctor/dr/disease/stage/controller/DiseaseStageController.java
@@ -0,0 +1,38 @@
+package com.doctor.dr.disease.stage.controller;
+
+import com.doctor.dr.disease.stage.dto.DiseaseStageDTO;
+import com.doctor.dr.disease.stage.service.DiseaseStageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/disease-stage")
+public class DiseaseStageController {
+    private final DiseaseStageService diseaseStageService;
+    public DiseaseStageController(DiseaseStageService diseaseStageService){
+        this.diseaseStageService=diseaseStageService;
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<DiseaseStageDTO>> getAll(){
+        List<DiseaseStageDTO> diseaseStageDTOList = this.diseaseStageService.getAll();
+        return ResponseEntity.ok(diseaseStageDTOList);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<DiseaseStageDTO> getById(@RequestParam("id") long id){
+        DiseaseStageDTO diseaseStageDTO = this.diseaseStageService.getById(id);
+        return ResponseEntity.ok(diseaseStageDTO);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<String> create(DiseaseStageDTO diseaseStageDTO){
+        this.diseaseStageService.create(diseaseStageDTO);
+        return ResponseEntity.ok("created");
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@RequestParam("id") long id){
+        this.diseaseStageService.deleteById(id);
+        return ResponseEntity.ok("deleted");
+    }
+}
