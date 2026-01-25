371276c6e135dc5d35551fea51c712c02e957968
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-25 23:29:25 +0530

    feat: Submission Repository.

---
+++ dr/src/main/java/com/doctor/dr/submission/repository/SubmissionRepository.java
@@ -0,0 +1,13 @@
+package com.doctor.dr.submission.repository;
+
+import com.doctor.dr.submission.entity.Submission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubmissionRepository extends JpaRepository<Submission,Long> {
+    List<Submission> findAllByIsActiveTrue();
+
+}
