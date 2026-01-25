b575842d677198d5ca1cf85cd0fb1a9e7c6a4d02
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-26 03:01:29 +0530

    feat: Disease stage Repository.

---
+++ dr/src/main/java/com/doctor/dr/disease/stage/repository/DiseaseStageRepository.java
@@ -0,0 +1,12 @@
+package com.doctor.dr.disease.stage.repository;
+
+import com.doctor.dr.disease.stage.entity.DiseaseStage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DiseaseStageRepository extends JpaRepository<DiseaseStage,Long> {
+    List<DiseaseStage> findAllByIsActiveTrue();
+}
