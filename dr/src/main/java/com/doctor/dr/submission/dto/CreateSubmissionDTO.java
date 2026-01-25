ff9f0ff70eb775dee395bfa5f5bbb0a0ffe81a1e
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-25 23:30:51 +0530

    feat: Special Submission DTO.

---
+++ dr/src/main/java/com/doctor/dr/submission/dto/CreateSubmissionDTO.java
@@ -0,0 +1,56 @@
+package com.doctor.dr.submission.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class CreateSubmissionDTO {
+    private long submissionId;
+
+    private String patientReferenceId;
+    private String createdDate;
+    private String createdTime;
+    private boolean isActive;
+    private boolean hasDisease;
+
+    private MultipartFile multipartFileImage;
+
+    public CreateSubmissionDTO() {
+    }
+
+    public CreateSubmissionDTO(long submissionId, String patientReferenceId, String createdDate, String createdTime, boolean isActive, boolean hasDisease,MultipartFile image) {
+        this.submissionId = submissionId;
+        this.patientReferenceId = patientReferenceId;
+        this.createdDate = createdDate;
+        this.createdTime = createdTime;
+        this.isActive = isActive;
+        this.hasDisease = hasDisease;
+        this.multipartFileImage =image;
+    }
+
+    public long getSubmissionId() {
+        return submissionId;
+    }
+
+    public String getPatientReferenceId() {
+        return patientReferenceId;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean hasDisease() {
+        return hasDisease;
+    }
+
+    public MultipartFile getMultipartFileImage() {
+        return multipartFileImage;
+    }
+}
