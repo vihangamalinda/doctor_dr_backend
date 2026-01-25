d1c9362bd536fa65415265ef4b763222bbdf3b43
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-25 23:30:30 +0530

    feat: Submission DTO.

---
+++ dr/src/main/java/com/doctor/dr/submission/dto/SubmissionDTO.java
@@ -0,0 +1,59 @@
+package com.doctor.dr.submission.dto;
+
+import com.doctor.dr.submission.entity.Submission;
+import jakarta.persistence.Column;
+
+public class SubmissionDTO {
+    private long submissionId;
+
+    private String patientReferenceId;
+    private String createdDate;
+    private String createdTime;
+    private boolean isActive;
+    private boolean hasDisease;
+
+    public SubmissionDTO() {
+    }
+
+    public SubmissionDTO(long submissionId, String patientReferenceId, String createdDate, String createdTime, boolean isActive, boolean hasDisease) {
+        this.submissionId = submissionId;
+        this.patientReferenceId = patientReferenceId;
+        this.createdDate = createdDate;
+        this.createdTime = createdTime;
+        this.isActive = isActive;
+        this.hasDisease = hasDisease;
+    }
+
+    public SubmissionDTO(Submission submission){
+        this.submissionId =submission.getSubmissionId();
+        this.patientReferenceId =submission.getPatientReferenceId();
+        this.createdDate =submission.getCreatedDate();
+        this.createdTime = submission.getCreatedTime();
+        this.hasDisease = submission.hasDisease();
+        this.isActive =submission.isActive();
+    }
+
+    public long getSubmissionId() {
+        return submissionId;
+    }
+
+    public String getPatientReferenceId() {
+        return patientReferenceId;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean hasDisease() {
+        return hasDisease;
+    }
+}
