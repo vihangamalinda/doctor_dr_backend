0dbc6c83a168a535bf2e06da0a27d953582797e3
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-29 21:35:39 +0530

    feat:hospital package related Hospital entity and DTO mapping related logic.

---
+++ dr/src/main/java/com/doctor/dr/hospital/mapper/HospitalMapper.java
@@ -0,0 +1,24 @@
+package com.doctor.dr.hospital.mapper;
+
+import com.doctor.dr.hospital.dto.request.HospitalRequestDTO;
+import com.doctor.dr.hospital.dto.response.HospitalResponseDTO;
+import com.doctor.dr.hospital.entity.Hospital;
+import com.doctor.dr.location.dto.LocationRequestDTO;
+import com.doctor.dr.location.dto.LocationResponseDTO;
+import com.doctor.dr.location.entity.Location;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Mapper(componentModel = "spring",uses = {LocationDetailMapper.class})
+public interface HospitalMapper {
+    @Mapping(source = "location",target = "location",qualifiedByName = "toLocationResponseDTO")
+    HospitalResponseDTO toHospitalResponseDTO(Hospital hospital);
+    @Mapping(source = "location",target = "location",qualifiedByName = "toLocation")
+    Hospital toHospital(HospitalRequestDTO hospitalRequestDTO);
+
+}
