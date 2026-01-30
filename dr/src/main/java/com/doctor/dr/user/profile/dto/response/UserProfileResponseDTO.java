4f2499d8fe0a18d3c206157f0dcfa878a504c3d8
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-01-31 05:04:25 +0530

    feat: User profile DTO for responses.

---
+++ dr/src/main/java/com/doctor/dr/user/profile/dto/response/UserProfileResponseDTO.java
@@ -0,0 +1,53 @@
+package com.doctor.dr.user.profile.dto.response;
+
+
+import java.time.LocalDateTime;
+
+public class UserProfileResponseDTO {
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final LocationDetailResponseDTO location;
+    private final boolean isActive;
+    private final LocalDateTime createdDateTime;
+
+    private final LocalDateTime modifiedDateTime;
+
+    public UserProfileResponseDTO(Long id, String firstName, String lastName, LocationDetailResponseDTO location, boolean isActive, LocalDateTime createdDateTime, LocalDateTime modifiedDateTime) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+        this.isActive = isActive;
+        this.createdDateTime = createdDateTime;
+        this.modifiedDateTime = modifiedDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocationDetailResponseDTO getLocation() {
+        return location;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public LocalDateTime getModifiedDateTime() {
+        return modifiedDateTime;
+    }
+}
