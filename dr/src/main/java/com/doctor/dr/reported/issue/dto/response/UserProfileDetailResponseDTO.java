4837cc0253a61e51e6846ff789de9c474cf67973
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-06 01:36:22 +0530

    feat: UserProfileDetail DTO responsible for response on reported-issue package.

---
+++ dr/src/main/java/com/doctor/dr/reported/issue/dto/response/UserProfileDetailResponseDTO.java
@@ -0,0 +1,13 @@
+package com.doctor.dr.reported.issue.dto.response;
+
+public class UserProfileDetailResponseDTO {
+    private final Long id;
+
+    public UserProfileDetailResponseDTO(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
