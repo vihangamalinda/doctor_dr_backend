42a8c538cedce43e0502a8ea9e10bbc77de86367
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-09 14:30:45 +0530

    feat: User Profile response DTO related to requested help package.

---
+++ dr/src/main/java/com/doctor/dr/requested/help/dto/response/UserProfileDetailResponseDTO.java
@@ -0,0 +1,13 @@
+package com.doctor.dr.requested.help.dto.response;
+
+public class UserProfileDetailResponseDTO {
+    private final Long id;
+
+    public UserProfileDetailResponseDTO(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
