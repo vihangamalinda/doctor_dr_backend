00ec6214a8f28024dfb76b991dca431d627869df
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-19 05:12:38 +0530

    feat:user-credential Request DTO.

---
+++ dr/src/main/java/com/doctor/dr/usercredential/dto/request/UserCredentialRegistrationRequestDTO.java
@@ -0,0 +1,29 @@
+package com.doctor.dr.usercredential.dto.request;
+
+import com.doctor.dr.user.profile.entity.UserProfile;
+
+
+public class UserCredentialRegistrationRequestDTO {
+    private final String userName;
+    private final UserProfile userProfile;
+    private final Long userRoleId;
+
+
+    public UserCredentialRegistrationRequestDTO(String userName, UserProfile userProfile, Long userRoleId) {
+        this.userName = userName;
+        this.userProfile = userProfile;
+        this.userRoleId = userRoleId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+}
