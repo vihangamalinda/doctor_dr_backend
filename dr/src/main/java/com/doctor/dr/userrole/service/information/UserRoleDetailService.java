af6a8f4b939ae45f2f96a60c1a11b26dcc99d210
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-19 05:07:07 +0530

    feat: User-role service that will be exposed to other packages.

---
+++ dr/src/main/java/com/doctor/dr/userrole/service/information/UserRoleDetailService.java
@@ -0,0 +1,7 @@
+package com.doctor.dr.userrole.service.information;
+
+import com.doctor.dr.userrole.entity.UserRole;
+
+public interface UserRoleDetailService {
+    UserRole getUserRoleById(long id);
+}
