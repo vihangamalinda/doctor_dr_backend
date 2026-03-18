757166d267d69ad78941eb6eb4c254941cb51073
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-19 05:22:32 +0530

    feat: web security configuration.

--- dr/src/main/java/com/doctor/dr/auth/WebConfig.java
+++ dr/src/main/java/com/doctor/dr/auth/WebConfig.java
@@ -1,11 +1,34 @@
 package com.doctor.dr.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -13,5 +36,42 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+//    .requestMatchers("/v1/user-profile/create").permitAll().
+//    request.
+//
+//                        anyRequest().authenticated()
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(request -> request
+                        .requestMatchers("/v1/user-profile/create","/v1/user-credential/login").permitAll().
+
+                        anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails vihanga = User.withUsername("Vihanga1").password("{noop}Vihanga1").roles("USER").build();
+//        UserDetails admin = User.withUsername("Admin").password("{noop}Admin").roles("ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(vihanga, admin);
+//    }
+
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return new BCryptPasswordEncoder(14);
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        return provider;
     }
 }
