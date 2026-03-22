81567f03e8c9c443383d3c10a0b3b4fb605310b0
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-22 23:27:22 +0530

    feat:Integrating the custom configuration.

--- dr/src/main/java/com/doctor/dr/DrApplication.java
+++ dr/src/main/java/com/doctor/dr/DrApplication.java
@@ -1,9 +1,12 @@
 package com.doctor.dr;
 
+import com.doctor.dr.internalsystemconfiguration.InternalSystemConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(InternalSystemConfiguration.class)
 public class DrApplication {
 
 	public static void main(String[] args) {
