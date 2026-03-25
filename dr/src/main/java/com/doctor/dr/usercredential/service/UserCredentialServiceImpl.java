0b13adc82e41ab0884362a128c49c868983e267d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-26 05:01:50 +0530

    feat:Integrating the cookie services inorder to produces a cookie after user's login request has been verified.

--- dr/src/main/java/com/doctor/dr/usercredential/service/UserCredentialServiceImpl.java
+++ dr/src/main/java/com/doctor/dr/usercredential/service/UserCredentialServiceImpl.java
@@ -1,10 +1,14 @@
 package com.doctor.dr.usercredential.service;
 
+import com.doctor.dr.auth.cookieservice.CookieService;
 import com.doctor.dr.usercredential.dto.request.UserLoginRequestDTO;
+import com.doctor.dr.usercredential.dto.response.AuthenticatedUserCredentialResponseDTO;
 import com.doctor.dr.usercredential.dto.response.UserCredentialResponseDTO;
 import com.doctor.dr.usercredential.entity.UserCredential;
 import com.doctor.dr.usercredential.repository.UserCredentialRepository;
 import com.doctor.dr.auth.jwtservice.JwtService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,11 +22,13 @@ public class UserCredentialServiceImpl implements UserCredentialService {
     private final UserCredentialRepository userCredentialRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final CookieService cookieService;
 
-    public UserCredentialServiceImpl(UserCredentialRepository userCredentialRepository, AuthenticationManager authenticationManager, JwtService jwtService) {
+    public UserCredentialServiceImpl(UserCredentialRepository userCredentialRepository, AuthenticationManager authenticationManager, JwtService jwtService, CookieService cookieService) {
         this.userCredentialRepository = userCredentialRepository;
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.cookieService = cookieService;
     }
 
     private UserCredential persistEntity(UserCredential userCredential) {
@@ -36,16 +42,40 @@ public class UserCredentialServiceImpl implements UserCredentialService {
     }
 
     @Override
-    public String login(UserLoginRequestDTO dto) {
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
-        Authentication authenticationToken= authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    public AuthenticatedUserCredentialResponseDTO login(UserLoginRequestDTO dto, HttpServletResponse httpServletResponse) {
+
+        Authentication authenticationToken = getAuthenticationToken(dto);
+
         if(authenticationToken.isAuthenticated()) {
-            System.out.println("User authenticated successfully.");
-            return jwtService.generateToken(dto.getUsername());
+            String username = dto.getUsername();
+
+            List<String> userRoleNames = getUserRoleNames(username);
+            String token =jwtService.generateToken(username);
+            Cookie cookie=cookieService.getCookieWithToken(token);
+            httpServletResponse.addCookie(cookie);
+
+            AuthenticatedUserCredentialResponseDTO authResponse=
+                    new AuthenticatedUserCredentialResponseDTO(token, username,userRoleNames);
+            return authResponse;
         }else {
             System.out.println("Authentication failed.");
-            return "failed";
+            return null;
         }
+    }
+
+    private List<String> getUserRoleNames(String username) {
+        UserCredential credential =findByUserName(username);
+        return retrieveUserRoleNames(credential);
+    }
+
+    private Authentication getAuthenticationToken(UserLoginRequestDTO dto) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    private  List<String> retrieveUserRoleNames(UserCredential credential) {
+        return credential.getRoleAssignments().stream().map(roleAssignment -> roleAssignment.getUserRole().getName()).toList();
     }
 
     private UserCredential findByUserName(String userName) {
